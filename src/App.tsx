1ac326caa8a9c58477a6fba0cd767023c5fe18f8
nocosharold <nocosharold@gmail.com>
2023-04-18 17:48:35 +0800

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import './App.css'
+import ExpenseFilter from './expense-tracker/components/ExpenseFilter';
+import ExpenseList from './expense-tracker/components/ExpenseList'
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.css'
+import ExpenseForm from './expense-tracker/components/ExpenseForm';
+import categories from './expense-tracker/categories';
+
+function App() {
+	const [selectedCategory, setSelectedCategory] = useState("");
+	const [expenses, setExpenses] = useState([
+		{ id: 1, description: 'aaa', amount: 10, category: 'Utilities' },
+		{ id: 2, description: 'bbb', amount: 10, category: 'Utilities' },
+		{ id: 3, description: 'ccc', amount: 10, category: 'Utilities' },
+		{ id: 4, description: 'ddd', amount: 10, category: 'Utilities' },
+	]);
+
+	const visibleExpenses = selectedCategory ? expenses.filter(e => e.category === selectedCategory) : expenses;
+
+	return (
+		<>
+
+			<div className="mb-5">
+				<ExpenseForm onSubmit={expense => setExpenses([...expenses, {...expense, id: expenses.length + 1 }])}/>
+			</div>
+			<div className="mb-3">
+				<ExpenseFilter onSelectCategory={category => setSelectedCategory(category)} />
+			</div>
+				<ExpenseList expenses={visibleExpenses} onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}/>
+		</>  
+	)
+}
+
+export default App

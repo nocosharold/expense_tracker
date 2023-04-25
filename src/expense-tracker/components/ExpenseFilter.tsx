c68e2d78a384fa965f7435ed7efe702933d25408
nocosharold <nocosharold@gmail.com>
2023-04-25 10:18:01 +0800

    update expense list

--- src/expense-tracker/components/ExpenseFilter.tsx
+++ src/expense-tracker/components/ExpenseFilter.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import categories from '../categories';
 
 interface Props {
@@ -6,12 +5,12 @@ interface Props {
 }
 
 const ExpenseFilter = ({ onSelectCategory }: Props) => {
-  return (
-    <select className="form-select" onChange={(event) => onSelectCategory(event.target.value)}>
-        <option value="">All categories</option>
-        {categories.map(category => <option key={category} value={category}>{category}</option>)}
-    </select>
-  )
+    return (
+        <select className="form-select" onChange={(event) => onSelectCategory(event.target.value)}>
+            <option value="">All categories</option>
+            {categories.map(category => <option key={category} value={category}>{category}</option>)}
+        </select>
+    )
 }
 
 export default ExpenseFilter
--- src/expense-tracker/components/ExpenseList.tsx
+++ src/expense-tracker/components/ExpenseList.tsx
@@ -17,33 +17,33 @@ const ExpenseList = ({ expenses, onDelete }: Props) => {
     if (expenses.length === 0) return null
     
     return (
-    <table className="table table-bordered">
-        <thead>
-            <tr>
-                <th>Description</th>
-                <th>Amount</th>
-                <th>Category</th>
-                <th></th>
-            </tr>
-        </thead>
-        <tbody>
-            {expenses.map(expense => <tr key={expense.id}>
-                <td>{expense.description}</td>
-                <td>{expense.amount}</td>
-                <td>{expense.category}</td>
-                <td><button className="btn btn-outline-danger" onClick={() => onDelete(expense.id)}>Delete</button></td>
-            </tr>)}
-        </tbody>
-        <tfoot>
-            <tr>
-                <td>Total</td>
-                <td>${expenses.reduce((acc, expense) => expense.amount + acc, 0).toFixed(2)}</td>
-                <td></td>
-                <td></td>
-            </tr>
-        </tfoot>
-    </table>
-  )
+        <table className="table table-bordered">
+            <thead>
+                <tr>
+                    <th>Description</th>
+                    <th>Amount</th>
+                    <th>Category</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                {expenses.map(expense => <tr className="align-middle" key={expense.id}>
+                    <td>{expense.description}</td>
+                    <td>{expense.amount}</td>
+                    <td>{expense.category}</td>
+                    <td><button className="btn btn-outline-danger" onClick={() => onDelete(expense.id)}>Delete</button></td>
+                </tr>)}
+            </tbody>
+            <tfoot>
+                <tr>
+                    <td>Total</td>
+                    <td>${expenses.reduce((acc, expense) => expense.amount + acc, 0).toFixed(2)}</td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tfoot>
+        </table>
+    )
 }
 
 export default ExpenseList
